cf3338693f38562cf38487307e6a385a819f450f
Ivan Kalesnik <115928976+LesnikBeats@users.noreply.github.com>
2024-03-07 23:34:49 +0300

    Hibernate DB

--- backend_store_application/api/src/main/java/External/Presentation/Controllers/UserController.java
+++ backend_store_application/api/src/main/java/External/Presentation/Controllers/UserController.java
@@ -3,13 +3,16 @@ package External.Presentation.Controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import store_application.Entities.User;
 import store_application.Webinars.Queries.Repositories.UserRepository;
 
 
+
 @RestController
+@RequestMapping(path = "/api")
 public class UserController {
 
     @Autowired
--- backend_store_application/api/src/main/java/store_application/Entities/User.java
+++ backend_store_application/api/src/main/java/store_application/Entities/User.java
@@ -2,24 +2,27 @@ package store_application.Entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
-@Data
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
+@Table(name = "users", schema = "user_database")
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Integer id;
 
     String email;
+
     String password;
 }

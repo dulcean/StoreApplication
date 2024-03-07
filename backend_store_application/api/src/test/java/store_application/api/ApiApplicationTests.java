acde2e4974881b61963202a0e9ff007ae612aa02
Ivan Kalesnik <115928976+LesnikBeats@users.noreply.github.com>
2024-03-07 20:03:30 +0300

    Commit backend

---
+++ backend_store_application/api/src/test/java/store_application/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package store_application.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
